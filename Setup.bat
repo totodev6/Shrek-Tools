2bf4c03c74bdd217d14b0800d763b0a8c614a683
blackray207 <147308962+blackray207@users.noreply.github.com>
2024-03-26 20:32:26 +0000

    Setup.bat

--- Setup.bat
+++ Setup.bat
@@ -2,7 +2,7 @@
 pip install colorama websocket selenium discord discum requests websocket pyautogui webbrowser pyinstaller ctypes qrcode python-http-client charset_normalizer 2captcha-python beautifulsoup4 pylibcheck pyperclip pypiwin32 packaging datetime pyfadecolor keyboard urlopen colored asyncio PyNaCL easygui tasksio pystyle Cipher colour Popen login pillow psutil emoji httpx loads tqdm pipe fore aes
 move utilities\Start.bat .
 cd utilities\assets
-start Pyinstaller.exe
+start pyinstaller.exe
 cd ..
 cd ..
 cls
